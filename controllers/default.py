58779a1db7797f1f4cdbf41211d311a8fde190f9
ChristianDiesta <64411885+ChristianDiesta@users.noreply.github.com>
2021-07-13 11:38:54 +0800

    first commit

---
+++ controllers/default.py
@@ -0,0 +1,20 @@
+def index():
+    return dict(message="Hello!")
+
+def page1():
+    return locals()
+    
+def page2():
+    return locals()
+    
+def page3():
+    return locals()
+
+def page4():
+    return locals()
+    
+def page5():
+    return locals()
+    
+def page6():
+    return locals()
