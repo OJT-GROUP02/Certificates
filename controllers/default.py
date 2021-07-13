e1a8d5ab02e6c228c5191ff08f7fd3f1c43de1b3
iveejntln <65966844+iveejntln@users.noreply.github.com>
2021-07-14 05:43:19 +0800

    page 5

--- controllers/default.py
+++ controllers/default.py
@@ -1,20 +1,66 @@
+from datetime import datetime as dt
+
+
 def index():
     return dict(message="Hello!")
 
+
+def suffix(d):
+    return 'th' if 11 <= d <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10,
+                                                                      'th')
+
+
+def custom_strftime(format_date, t):
+    return t.strftime(format_date).replace('{S}', str(t.day) + suffix(t.day))
+
+
 def page1():
     return locals()
-    
+
+
 def page2():
     return locals()
-    
+
+
 def page3():
     return locals()
 
+
 def page4():
     return locals()
-    
+
+
 def page5():
+    current_day = custom_strftime('{S}', dt.now())
+    current_month_year = custom_strftime('%B, %Y', dt.now())
+
+    header = db(db.campus_college_institute.cci_id == 10).select(
+        db.campus_college_institute.cci_name,
+        db.campus_college_institute.address,
+        db.campus_college_institute.tel_no)
+
+    student = db((db.undergrad_stud.first_name == 'Francia') |
+                 (db.undergrad_stud.last_name == 'Llaban')).select(
+        db.undergrad_stud.first_name,
+        db.undergrad_stud.middle_name,
+        db.undergrad_stud.last_name,
+        db.undergrad_stud.gender,
+        db.undergrad_stud.date_graduated,
+        db.degree_courses.course_name,
+        db.degree_courses.course_abbrev,
+        db.majors.major,
+        left=[db.degree_courses.on(db.undergrad_stud.course_id ==
+                                  db.degree_courses.course_id),
+              db.majors.on(db.undergrad_stud.major_id == db.majors.major_id)])
+
+    date_graduated = custom_strftime('%B %d, %Y', student[
+        0].undergrad_stud.date_graduated)
+
+    registrar = db((db.registrar.registrar_position == 'Registrar II')).select(
+        db.registrar.registrar_name)
+
     return locals()
-    
+
+
 def page6():
     return locals()
