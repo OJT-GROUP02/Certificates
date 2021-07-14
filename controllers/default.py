338ffad2c7b56df7537745bb82094590107980ef
vricaa <79438707+vricaa@users.noreply.github.com>
2021-07-14 15:37:31 +0800

    page 1 and 2 update

--- controllers/default.py
+++ controllers/default.py
@@ -15,10 +15,56 @@ def custom_strftime(format_date, t):
 
 
 def page1():
+    header = db(db.campus_college_institute.cci_id == 11).select(
+        db.campus_college_institute.cci_name,
+        db.campus_college_institute.address,
+        db.campus_college_institute.tel_no)
+    
+    student = db(db.grad_stud.student_id == 1).select(
+        db.grad_stud.first_name,
+        db.grad_stud.middle_name,
+        db.grad_stud.last_name,
+        db.grad_stud.gender,
+        db.degree_courses.course_name,
+        db.degree_courses.course_abbrev,
+        db.majors.major,
+        left=[db.degree_courses.on(db.grad_stud.course_id ==
+                                  db.degree_courses.course_id),
+              db.majors.on(db.grad_stud.major_id == db.majors.major_id)])
+
+    current_day = custom_strftime('{S}', dt.now())
+    current_month_year = custom_strftime('%B, %Y', dt.now())
+
+    registrar = db((db.registrar.registrar_position == 'Registrar II')).select(
+        db.registrar.registrar_name)
+
+    registrar = db((db.registrar.registrar_position == 'University Registrar')).select(
+        db.registrar.registrar_name)
+
     return locals()
 
 
 def page2():
+    student = db(db.undergrad_stud.student_id == 1).select(
+        db.undergrad_stud.first_name,
+        db.undergrad_stud.middle_name,
+        db.undergrad_stud.last_name,
+        db.undergrad_stud.gender,
+        db.undergrad_stud.date_graduated,
+        db.degree_courses.course_name,
+        db.degree_courses.course_abbrev,
+        left=[db.degree_courses.on(db.undergrad_stud.course_id ==
+                                  db.degree_courses.course_id)
+
+    date_graduated = custom_strftime('%B %d, %Y', student[
+        0].undergrad_stud.date_graduated)
+
+    current_day = custom_strftime('{S}', dt.now())
+    current_month_year = custom_strftime('%B, %Y', dt.now())
+
+    registrar = db((db.registrar.registrar_position == 'University Registrar')).select(
+        db.registrar.registrar_name)
+
     return locals()
 
 
